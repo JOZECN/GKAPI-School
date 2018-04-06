c95e29e6446c4e4bc3fb429d2a54a49599991964
周高爽 <1902348023@qq.com>
2018-04-06 12:55:27 +0800

    Jozecn
    修改login页面css样式

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,11 @@ $(function() {
         $('.flip-item-front').css("transform","rotateY(180deg)");
         $('.flip-item-back').css("z-index","2");
         $('.flip-item-back').css("transform","rotateY(0deg)");
-        $('.flip').css("top","calc(45% - 200px)");
-        $('.flip').css("left","calc(50% - 300px)")
+    })
+    $('.login').click(function(){
+        $('.flip-item-front').css("z-index","2");
+        $('.flip-item-front').css("transform","rotateY(0deg)");
+        $('.flip-item-back').css("z-index","1");
+        $('.flip-item-back').css("transform","rotateY(180deg)");
     })
 })
